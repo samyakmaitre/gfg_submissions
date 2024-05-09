8980f22cf82aa9447f5354fe9007a4f93f4954e5
Samyak Maitre <130790017+samyakmaitre@users.noreply.github.com>
2024-05-09 11:46:42 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Divisor Game/divisor-game.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.math.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            Solution obj = new Solution();
+            String ans = obj.divisorGame(n) ? "True" : "False";
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public static boolean divisorGame(int n) {
+        int count = 0;
+        while (n > 0) {
+            boolean divisorFound = false;
+            for (int x = 1; x < n; x++) {
+                if (n % x == 0) {
+                    n -= x;
+                    divisorFound = true;
+                    break;
+                }
+            }
+            if (!divisorFound) {
+                break;
+            }
+            count++;
+        }
+        return count % 2 != 0;
+    }
+}
