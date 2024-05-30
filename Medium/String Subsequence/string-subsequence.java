f340ba66a8da68be8cb1351c6e80b8047d0efa74
Samyak Maitre <130790017+samyakmaitre@users.noreply.github.com>
2024-05-30 22:55:59 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/String Subsequence/string-subsequence.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            String s1;
+            s1 = br.readLine();
+
+            String s2;
+            s2 = br.readLine();
+
+            Solution obj = new Solution();
+            int res = obj.countWays(s1, s2);
+
+            System.out.println(res);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    static int dp[][];
+    static int mod = 1000000007;
+    public static int countWays(String s1, String s2) {
+
+        int n = s1.length(), m=s2.length();
+        dp = new int[n+1][m+1];
+      for(int i=1; i<=m; i++){
+          dp[0][i]=0;
+      }
+      for(int i=0; i<=n;i++)
+      dp[i][0]=1;
+
+      for(int i=1;i<=n; i++){
+          for(int j=1; j<=m; j++){
+              if(s1.charAt(i-1)==s2.charAt(j-1))
+              dp[i][j] = (dp[i-1][j-1]%mod + dp[i-1][j]%mod)%mod;
+
+              else
+                dp[i][j] =  dp[i-1][j]; 
+          }
+      }
+
+      return  dp[n][m] ;
+    }
+
+}
