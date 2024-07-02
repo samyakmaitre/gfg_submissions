4596d9fa80ffd34cacb87f80507085227dca326e
Samyak Maitre <130790017+samyakmaitre@users.noreply.github.com>
2024-07-02 23:32:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Linked list of strings forms a palindrome/linked-list-of-strings-forms-a-palindrome.java
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node {
+    String data;
+    Node next;
+
+    Node(String x) {
+        data = x;
+        next = null;
+    }
+}
+
+class LinkedList {
+    Node head;
+
+    LinkedList() { head = null; }
+
+    void addNode(String str) {
+        if (head == null) {
+            head = new Node(str);
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = new Node(str);
+        }
+    }
+
+    void print() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+            int K = sc.nextInt();
+            LinkedList list = new LinkedList();
+
+            for (int i = 0; i < K; i++) {
+                String str = sc.next();
+                list.addNode(str);
+            }
+
+            Solution ob = new Solution();
+            boolean ans = ob.compute(list.head);
+            if (ans) {
+                System.out.println("true");
+            } else {
+                System.out.println("false");
+            }
+        }
+        sc.close();
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    public boolean compute(Node root) {
+        // Step 1: Concatenate the strings from the linked list
+        StringBuilder combinedString = new StringBuilder();
+        Node current = root;
+        while (current != null) {
+            combinedString.append(current.data);
+            current = current.next;
+        }
+
+        // Step 2: Check if the combined string is a palindrome
+        String original = combinedString.toString();
+        String reversed = combinedString.reverse().toString();
+        return original.equals(reversed);
+    }
+}
