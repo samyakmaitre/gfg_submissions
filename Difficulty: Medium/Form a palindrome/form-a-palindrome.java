56ffcf7ec25988bac06dcfe4c39dead1b5f0c654
Samyak Maitre <130790017+samyakmaitre@users.noreply.github.com>
2024-07-27 19:15:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Form a palindrome/form-a-palindrome.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*; 
+
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0){
+            String str = read.readLine();
+
+            Solution ob = new Solution();
+            
+            System.out.println(ob.countMin(str));
+        }
+    } 
+} 
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int countMin(String str) {
+        int n = str.length();
+        // Create a DP array to store the length of LPS
+        int[][] dp = new int[n][n];
+        
+        // Every single character is a palindrome of length 1
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = 1;
+        }
+        
+        // Build the table. cl is the length of substring.
+        for (int cl = 2; cl <= n; cl++) {
+            for (int i = 0; i < n - cl + 1; i++) {
+                int j = i + cl - 1;
+                if (str.charAt(i) == str.charAt(j) && cl == 2) {
+                    dp[i][j] = 2;
+                } else if (str.charAt(i) == str.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                } else {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i + 1][j]);
+                }
+            }
+        }
+        
+        // The length of LPS is dp[0][n-1]
+        int lps = dp[0][n - 1];
+        
+        // The minimum insertions needed is the difference between the
+        // length of the string and the length of the longest palindromic subsequence
+        return n - lps;
+    }
+}
