9a2425b432609424dba70c82c1d1f7d7e5ca3b4d
Samyak Maitre <130790017+samyakmaitre@users.noreply.github.com>
2024-08-04 22:51:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/N meetings in one room/n-meetings-in-one-room.java
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while (t-- > 0) {
+            String inputLine[] = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+
+            int start[] = new int[n];
+            int end[] = new int[n];
+
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) start[i] = Integer.parseInt(inputLine[i]);
+
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) end[i] = Integer.parseInt(inputLine[i]);
+
+            int ans = new Solution().maxMeetings(n, start, end);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+class Solution {
+    public int maxMeetings(int n, int start[], int end[]) {
+        Meeting[] meetings = new Meeting[n];
+        for (int i = 0; i < n; i++) {
+            meetings[i] = new Meeting(start[i], end[i]);
+        }
+
+        Arrays.sort(meetings, new Comparator<Meeting>() {
+            @Override
+            public int compare(Meeting m1, Meeting m2) {
+                return m1.end - m2.end;
+            }
+        });
+
+        int count = 1;
+        int lastEndTime = meetings[0].end;
+
+        for (int i = 1; i < n; i++) {
+            if (meetings[i].start > lastEndTime) {
+                count++;
+                lastEndTime = meetings[i].end;
+            }
+        }
+
+        return count;
+    }
+
+    class Meeting {
+        int start;
+        int end;
+
+        Meeting(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+    }
+}
