9aec02dd94b7669968364b2c239b01aa286ed3d4
Samyak Maitre <130790017+samyakmaitre@users.noreply.github.com>
2024-09-26 22:47:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Roof Top/roof-top.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class gfg {
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+            String st[] = read.readLine().trim().split("\\s+");
+            int n = st.length;
+            int arr[] = new int[n];
+
+            for (int i = 0; i < n; i++) arr[(int)i] = Integer.parseInt(st[(int)i]);
+
+            System.out.println(new Solution().maxStep(arr));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public int maxStep(int arr[]) {
+        int maxSteps = 0;
+        int currentSteps = 0;
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > arr[i - 1]) {
+                currentSteps++;
+            } else {
+                maxSteps = Math.max(maxSteps, currentSteps);
+                currentSteps = 0;
+            }
+        }
+
+        maxSteps = Math.max(maxSteps, currentSteps);
+
+        return maxSteps;
+    }
+}
